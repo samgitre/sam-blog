088db530c90f6a00dcdfa6e4b8a9510cb870aea3
samgitre <samgitrepos@gmail.com>
2017-02-16 17:23:46 +0700

    Update dbConfig.js

--- configs/database/config/dbConfig.js
+++ configs/database/config/dbConfig.js
@@ -1,12 +1,1 @@
-var mongoose = require('mongoose');
-var url = "mongodb://bitsy_soft:dy13lsd7425@ds139879.mlab.com:39879/bitsyblog";
 
-mongoose.Promise = global.Promise;
-
-mongoose.connect(url, function() {
-  mongoose.connection.once('open', function() {
-    console.log("Connected to data house successfully");
-  }).on('Error', function (error) {
-    console.warn("Unable to connect to server" + error);
-  });
-});
